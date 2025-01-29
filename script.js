8da4b3f67ec7139924adae630510d52e41271644
Pierre Boisnard <pierre.boisnard@live.fr>
2025-01-29 13:48:56 +0100

    update front

--- script.js
+++ script.js
@@ -1,71 +1,62 @@
 const MYURL = "http://localhost:3000";
 
-document.querySelector("#btn-search").addEventListener("click", async (e) => {
-  let data = {
-    departure: document.querySelector("#search-departure").value,
-    arrival: document.querySelector("#search-arrival").value,
-    date: document.querySelector("#travelDate").value,
-  };
-  //console.log(data);
+document.querySelector('#travelDate').min = new Date().toISOString().split('T')[0];
+document.querySelector('#tripsList').style.display = 'none';
 
-  const response = await fetch(`${MYURL}/trips/search`, {
+function appendTripsSearched(trips) {
+    if (trips.length > 0) {
+        document.querySelector('#contents').style.display = 'none';
+        document.querySelector('#tripsList').style.display = 'initial';
+        document.querySelector("#tripsList").innerHTML = "";
+
+    for (const trip of trips) {
+        document.querySelector("#tripsList").innerHTML += `
+            <div class="trips">
+                <p class="trip">${trip.arrival} > ${trip.departure}</p>
+                <p class="tripHour">${trip.hours}</p>
+                <p class="tripPrice">${trip.price}</p>
+                <button type="button" class="bookTrip-btn" id="bookTrip-btn" data-tripid="${trip._id}">Book</button>
+            </div>
+        `;
+    }
+} else {
+    document.querySelector("#contents").style.display = 'initial';
+    document.querySelector("#tripsList").style.display = 'none';
+
+    document.querySelector("#image > img").src = './images/notfound.png';
+    document.querySelector("#text > p").textContent = 'No trip found.';
+}
+}
+
+document.querySelector('#btn-search').addEventListener('click', async () => {
+    let data = {
+        departure: document.querySelector("#search-departure").value,
+        arrival: document.querySelector("#search-arrival").value,
+        date: document.querySelector("#travelDate").value,
+    };
+
+const response = await fetch(`${MYURL}/trips/search`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
-  });
-
-  data = await response.json();
-
-  if (data.trips.length > 0) {
-    document.querySelector("#contents").innerHTML = "";
-    document.querySelector("#tripsList").innerHTML = "";
-    for (let i = 0; i < data.trips.length; i++) {
-      document.querySelector("#tripsList").innerHTML += `
-        <div class="trips">
-            <p class="trip">${data.trips[i].arrival} > ${data.trips[i].departure}</p>
-            <p class="tripHour">${data.trips[i].hours}</p>
-            <p class="tripPrice">${data.trips[i].price}</p>
-            <button type="button" id="bookTrip-btn" data-tripid="${data.trips[i]._id}">Book</button>
-        </div>
-        `;
-        //depart et arrivé inversé
-    }
-    /* document.querySelector("#image").style.display = "none";
-    document.querySelector("#text").style.display = "none";
-    document.querySelector("#lign").style.display = "none"; */
-  } else {
-    document.querySelector("#contents").innerHTML = "";
-    document.querySelector("#tripsList").innerHTML = "";
-    document.querySelector("#contents").innerHTML += `
-        <div id="image">
-            <img src="./images/notfound.png" alt="train">
-        </div>
-        <hr id="lign">
-        <div id="text">
-           <p>No trip found.</p>
-       </div>
-        `
-  }
-
-for (let i = 0; i < document.querySelectorAll("#bookTrip-btn").length; i++) {
-    document.querySelectorAll('#bookTrip-btn')[i].addEventListener('click', async (e) => {
-        
-        let tripID = e.target.dataset.tripid
-
-        let data = {
-            tripID: tripID,
-          };
-          console.log(data);
-
-        const response = await fetch(`${MYURL}/carts/save`, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(data),
-          });
-        data = await response.json();
-        window.location.href = 'cart.html'
-        document.querySelector("#content").innerHTML = "";
-        })
-    }
 });
 
+data = await response.json();
+
+appendTripsSearched(data.trips);
+}); 
+
+document.querySelector('#tripsList').addEventListener('click', async (e) => {
+    const target = e.target;
+
+if (target.classList.contains('bookTrip-btn')) {
+    const response = await fetch(`${MYURL}/carts/save`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ tripID: target.dataset.tripid }),
+    });
+
+    data = await response.json();
+    window.location.assign('cart.html');
+}
+});
